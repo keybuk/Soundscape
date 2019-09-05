3c4bedbbc37312600cfed23e3565b297cd5e8e33
Scott James Remnant <scott@netsplit.com>
2019-09-05 10:42:51 -0700

    Initial Commit

---
+++ Soundscape/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Soundscape
+//
+//  Created by Scott James Remnant on 9/5/19.
+//  Copyright © 2019 Scott James Remnant. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
