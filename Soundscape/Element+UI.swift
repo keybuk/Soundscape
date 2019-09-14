ca4d97960c55f45b99dca528208b854934c04a57
Scott James Remnant <scott@netsplit.com>
2019-09-13 19:27:55 -0700

    Add Identifiable to Element

---
+++ Soundscape/Element+UI.swift
@@ -0,0 +1,11 @@
+//
+//  Element+UI.swift
+//  Soundscape
+//
+//  Created by Scott James Remnant on 9/13/19.
+//  Copyright © 2019 Scott James Remnant. All rights reserved.
+//
+
+import Foundation
+
+extension Element: Identifiable {}
