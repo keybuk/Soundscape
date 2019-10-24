b2e16a7d22986becd1cb8c1f7f85f44afb359aaf
Scott James Remnant <scott@netsplit.com>
2019-10-24 10:04:39 -0700

    Disable the idle timer when we're in the foreground
    Background is okay because we have the right background mode, I think

--- Soundscape/SceneDelegate.swift
+++ Soundscape/SceneDelegate.swift
@@ -56,12 +56,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
+
+        UIApplication.shared.isIdleTimerDisabled = true
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+
+        UIApplication.shared.isIdleTimerDisabled = false
     }
 
 
