3633f9b58fbe702b183a41fca899e4cf2f0d0596
Scott James Remnant <scott@netsplit.com>
2019-11-12 19:41:21 -0800

    Add support for playing looping files
    This has to be opted-in by the player and will queue both the initial section, and looping section, with the looping section set with the .loops option
    
    If the file doesn't have a looping section, it'll queue a zero-length initial section and loop the whole file.

--- Soundscape/AVAudioPlayerNode+OggVorbis.swift
+++ Soundscape/AVAudioPlayerNode+OggVorbis.swift
@@ -9,11 +9,16 @@
 import Foundation
 import AVFoundation
 
-extension AVAudioPlayerNode {
-    func scheduleFile(_ file: OggVorbisFile, at when: AVAudioTime? = nil, startHandler: AVAudioNodeCompletionHandler? = nil, completionHandler: AVAudioNodeCompletionHandler? = nil) {
-        // FIXME: this limits our play time to around 13 hours... oh no.
-        let frames = AVAudioFrameCount(truncatingIfNeeded: file.length)
+extension ClosedRange where Bound == AVAudioFramePosition {
+    var length: AVAudioFramePosition {
+        AVAudioFramePosition(truncatingIfNeeded: count)
+    }
+}
 
+extension AVAudioPlayerNode {
+    func scheduleFile(_ file: OggVorbisFile, looping: Bool = false, at when: AVAudioTime? = nil, startHandler: AVAudioNodeCompletionHandler? = nil, completionHandler: AVAudioNodeCompletionHandler? = nil) {
+        // FIXME: this limits our play time to around 13 hours... oh no.
+        let frames = AVAudioFrameCount(truncatingIfNeeded: looping ? (file.loop?.lowerBound ?? 0) : file.length)
         guard let buffer = AVAudioPCMBuffer(pcmFormat: file.fileFormat, frameCapacity: frames) else {
             print("Failed to allocate buffer")
             completionHandler?()
@@ -21,27 +26,56 @@ extension AVAudioPlayerNode {
         }
 
         do {
-            try file.read(into: buffer)
+            try file.read(into: buffer, frameCount: frames)
         } catch let error {
             print("Failed to read into buffer: \(error.localizedDescription)")
             completionHandler?()
             return
         }
 
-        if buffer.frameLength > 0 {
-            if startHandler != nil {
-                if let startBuffer = AVAudioPCMBuffer(pcmFormat: file.fileFormat, frameCapacity: 0) {
-                    scheduleBuffer(startBuffer, at: when, completionHandler: startHandler)
-                } else {
-                    print("Failed to allocate buffer, calling start handler early")
-                    startHandler?()
-                }
+        // Load the looping portion.
+        // When looping, if the file has no loop section, the initial buffer is zero-length and this
+        // is the buffer we actually end up playing. When the file has a loop section, we play both
+        // and loop this one.
+        let loopBuffer: AVAudioPCMBuffer?
+        if looping {
+            let loopFrames = AVAudioFrameCount(truncatingIfNeeded: file.loop?.length ?? file.length)
+            loopBuffer = AVAudioPCMBuffer(pcmFormat: file.fileFormat, frameCapacity: loopFrames)
+            guard loopBuffer != nil else {
+                print("Failed to allocate loop buffer")
+                completionHandler?()
+                return
             }
 
-            scheduleBuffer(buffer, at: when, completionHandler: completionHandler)
+            do {
+                try file.read(into: loopBuffer!, frameCount: loopFrames)
+            } catch let error {
+                print("Failed to read into loop buffer: \(error.localizedDescription)")
+                completionHandler?()
+                return
+            }
         } else {
-            startHandler?()
-            completionHandler?()
+            loopBuffer = nil
+        }
+
+        if startHandler != nil {
+            if let startBuffer = AVAudioPCMBuffer(pcmFormat: file.fileFormat, frameCapacity: 0) {
+                scheduleBuffer(startBuffer, at: when, completionHandler: startHandler)
+            } else {
+                print("Failed to allocate buffer, calling start handler early")
+                startHandler?()
+            }
+        }
+
+        if let loopBuffer = loopBuffer {
+            let loopWhen = when.map {
+                AVAudioTime(sampleTime: $0.sampleTime + (file.loop?.lowerBound ?? 0), atRate: file.processingFormat.sampleRate)
+            }
+
+            scheduleBuffer(buffer, at: when)
+            scheduleBuffer(loopBuffer, at: loopWhen, options: .loops, completionHandler: completionHandler)
+        } else {
+            scheduleBuffer(buffer, at: when, completionHandler: completionHandler)
         }
     }
 }
